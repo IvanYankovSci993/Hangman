fee08ca65968626a321c613e8679701cd1ee2264
IvanYankovSci993 <ivan.yankov.sci@gmail.com>
2023-10-18 19:17:25 +0100

    Milestone_2 expanded

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,11 @@
+import random
 word_list = ["bannana", "apple", "orange", "kiwi", "mango"]
-print(word_list)
+print((word_list))
+word=random.choice(word_list)
+print(word)
+print("Guess a letter:")
+guess=input()
+if len(guess)==1 and guess.isalnum():
+        print(guess)
+else:
+        print("Oops! That is not a valid input")
