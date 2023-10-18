32238e0348612542cf94f3ecc17ae256caf82453
IvanYankovSci993 <ivan.yankov.sci@gmail.com>
2023-10-18 19:22:28 +0100

    Comments

--- milestone_2.py
+++ milestone_2.py
@@ -3,8 +3,10 @@ word_list = ["bannana", "apple", "orange", "kiwi", "mango"]
 print((word_list))
 word=random.choice(word_list)
 print(word)
+# ask the user to guess a letter
 print("Guess a letter:")
 guess=input()
+# check if the guess is 1 character long and if it is alnum
 if len(guess)==1 and guess.isalnum():
         print(guess)
 else:
